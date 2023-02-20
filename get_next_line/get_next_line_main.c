d3c31bfeea256cb53b9437adafe9c2884fc10e70
TTmarina <115308880+TTmarina@users.noreply.github.com>
2023-02-20 17:59:22 +0100

    gnl

---
+++ get_next_line/get_next_line_main.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_main.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmarina- <tmarina-@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 17:52:01 by tmarina-          #+#    #+#             */
+/*   Updated: 2023/02/20 17:52:59 by tmarina-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 42
+
+char *ft_get_next_line(int fd);
+
+int main(int argc, char **argv)
+{
+    int fd;
+    char *line;
+
+    if (argc != 2)
+    {
+        printf("usage: %s file\n", argv[0]);
+        return (1);
+    }
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
+    {
+        perror("Error opening file");
+        return (1);
+    }
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s\n", line);
+        free(line);
+    }
+    close(fd);
+    return (0);
+}
